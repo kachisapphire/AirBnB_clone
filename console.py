5a2ccad0524f3f02515672edc938a2179ac13aa7
kachisapphire <mychaelesther99@gmail.com>
2024-07-22 09:08:58 -0700

    console

---
+++ console.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" that contains the entry point of the command interpreter """
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    """ my class definition """
+    prompt = "(hbnb)"
+
+    def do_quit(self,arg):
+        """ to exit the program"""
+        return True
+
+    def help(self, arg):
+        """ (this action is provided by default) """
+        print("Quit command to exit the program")
+
+    def do_EOF(self, arg):
+        """ end of file """
+        print()
+        return True
+
+if __name__ == "__main__":
+    HBNBCommand().cmdloop()
